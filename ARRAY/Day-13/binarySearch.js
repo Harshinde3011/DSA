0f374fb5da401ad188b669104c67cd608cde8978
Harshh@57# <harshwardhan.shinde@ifasonline.com>
2025-11-20 23:41:13 +0530

    binary search

---
+++ ARRAY/Day-13/binarySearch.js
@@ -0,0 +1,23 @@
+const arr = [10, 20, 30, 40, 50, 60, 70];
+
+function binarySearch(arr, start, end, target) {
+    if (start > end) {
+        return -1;
+    }
+
+    let mid = Math.floor((start+end)/2);
+    console.log(`start is ${start} end is ${end} and mid is ${arr[mid]}`);
+    
+    if (arr[mid] == target) {
+        return arr[mid];
+    }else if (target < arr[mid]) {
+        console.log(`mid ${arr[mid]} is greater than ${target}`);
+        return binarySearch(arr, start, mid - 1, target);
+    }else if(target > arr[mid]){
+        
+        console.log(`mid ${arr[mid]} is less than ${target}`);
+        return binarySearch(arr, mid + 1, end, target)
+    }
+}
+
+console.log(binarySearch(arr, 0, arr.length - 1, 50));
