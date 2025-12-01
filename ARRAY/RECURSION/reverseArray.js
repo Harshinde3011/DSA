4fb9f3a6befcaedb41af56ff6ae51eb32a2a0980
Harshh@57# <harshwardhan.shinde@ifasonline.com>
2025-12-01 15:17:36 +0530

    reverse array using one variable

--- ARRAY/RECURSION/reverseArray.js
+++ ARRAY/RECURSION/reverseArray.js
@@ -1,15 +1,15 @@
 const nums = [1,2,3,4,5,6];
 
-function reverseArray(nums, s, e) {
-    if (s >= e) {
+function reverseArray(nums, s, n) {
+    if (s >= Math.floor(n/2)) {
         return nums;
     }
 
     let temp = nums[s];
-    nums[s] = nums[e];
-    nums[e] = temp;
+    nums[s] = nums[n-1-s];
+    nums[n-1-s] = temp;
 
-    return reverseArray(nums, s+1, e-1);
+    return reverseArray(nums, s+1, n);
 }
 
-console.log(reverseArray(nums, 0, nums.length - 1));
+console.log(reverseArray(nums, 0, nums.length));
