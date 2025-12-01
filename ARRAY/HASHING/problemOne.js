a462c480d8b1982ec53c9234fb99c6f508b8aa98
Harshh@57# <harshwardhan.shinde@ifasonline.com>
2025-12-01 15:40:29 +0530

    find occurances of numbers in array

---
+++ ARRAY/HASHING/problemOne.js
@@ -0,0 +1,15 @@
+const array = [1,2,1,3,2,4,5]
+
+function numberOfOccrances(nums) {
+    let hashMap = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const element = nums[i];
+            
+        hashMap.set(element, ((hashMap.get(element) || 0) + 1))
+    }
+
+    return hashMap;
+}
+
+console.log(numberOfOccrances(array));
