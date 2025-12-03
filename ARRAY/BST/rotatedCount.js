090649cb48bfaf8f966d6e0dcad8650c69295bbf
Harshh@57# <harshwardhan.shinde@ifasonline.com>
2025-12-03 19:00:16 +0530

    return ratated count for rotated array

--- ARRAY/BST/rotatedCount.js
+++ ARRAY/BST/rotatedCount.js
@@ -1,8 +1,9 @@
-// find min from below roated array, ans is 10
+// how many times the array has been rotated
 const arr = [40, 50, 60, 70, 10, 20, 30];
 
-function findMinFromArray(nums) {
+function getRotatedCount(nums) {
     let ans = Infinity;
+    let pivot = Infinity;
     let start = 0;
     let end = nums.length - 1;
 
@@ -11,21 +12,24 @@ function findMinFromArray(nums) {
 
         if (nums[start] <= nums[end]) {
             ans = Math.min(ans, nums[start]);
+            pivot = start;
             break;
         }
         
         if (nums[start] <= nums[mid]) {
             ans = Math.min(ans, nums[start]);
+            pivot = start;
             start = mid + 1;
         }else{
             if (nums[mid] < ans) {
                 ans = Math.min(ans, nums[mid]);
+                pivot = mid;
                 end = mid - 1;
             }
         }
     }
 
-    return ans;
+    return pivot;
 }
 
-console.log(findMinFromArray(arr));
+console.log(getRotatedCount(arr));
