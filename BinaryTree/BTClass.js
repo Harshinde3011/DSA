2467a29faf1777ea0bf1750b79d186bf50b70fb4
Harshh@57# <harshwardhan.shinde@ifasonline.com>
2025-12-28 15:34:15 +0530

    Binary search TREE

---
+++ BinaryTree/BTClass.js
@@ -0,0 +1,119 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    isEmpty() {
+        return this.root === null;
+    }
+
+    insert(value) {
+        console.log(`\nInserting ${value} into BST`);
+
+        const newNode = new Node(value);
+
+        if (this.isEmpty()) {
+            console.log(`Tree is empty. Setting ${value} as root`);
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(root, newNode) {
+        console.log(
+            `Comparing newNode (${newNode.value}) with root (${root.value})`
+        );
+
+        // Go LEFT
+        if (newNode.value < root.value) {
+            console.log(`${newNode.value} < ${root.value} → Go LEFT`);
+
+            if (root.left === null) {
+                console.log(`Left child is empty. Inserting ${newNode.value}`);
+                root.left = newNode;
+            } else {
+                console.log(`Moving to left child (${root.left.value})`);
+                this.insertNode(root.left, newNode);
+            }
+
+        } 
+        // Go RIGHT
+        else {
+            console.log(`${newNode.value} >= ${root.value} → Go RIGHT`);
+
+            if (root.right === null) {
+                console.log(`Right child is empty. Inserting ${newNode.value}`);
+                root.right = newNode;
+            } else {
+                console.log(`Moving to right child (${root.right.value})`);
+                this.insertNode(root.right, newNode);
+            }
+        }
+    }
+
+    search(root, value){
+        if (!root) {
+            return false;
+        }else{
+            if (root.value === value) {
+                return true;
+            }else if (value < root.value) {
+                return this.search(root.left, value)
+            }else{
+                return this.search(root.right, value)
+            }
+        }
+    }
+
+    preOrder(root){
+        if (root) {
+            console.log(root.value);
+            this.preOrder(root.left);
+            this.preOrder(root.right);
+        }
+    }
+
+    inOrder(root) {
+        if (root) {
+            this.preOrder(root.left);
+            console.log(root.value);
+            this.preOrder(root.right);
+        }
+    }
+
+    postOrder(root) {
+        if (root) {
+            this.preOrder(root.left);
+            this.preOrder(root.right);
+            console.log(root.value);
+        }
+    }
+}
+
+
+const bst = new BinarySearchTree();
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+
+
+console.log(bst.search(bst.root, 10));
+console.log(bst.search(bst.root, 5));
+console.log(bst.search(bst.root, 15));
+console.log(bst.search(bst.root, 30));
+
+console.log("pre order traversal: ", bst.preOrder(bst.root));
+console.log("In order traversal: ", bst.inOrder(bst.root));
+console.log("post order traversal: ", bst.postOrder(bst.root));
---
+++ BinaryTree/DFS.js
@@ -0,0 +1,88 @@
+class BinarySearchTree {
+    constructor(parameters) {
+        this.root = null;
+    }
+
+    isEmpty() {
+        return this.root === null;
+    }
+
+    insert(value) {
+        console.log(`\nInserting ${value} into BST`);
+
+        const newNode = new Node(value);
+
+        if (this.isEmpty()) {
+            console.log(`Tree is empty. Setting ${value} as root`);
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(root, newNode) {
+        console.log(
+            `Comparing newNode (${newNode.value}) with root (${root.value})`
+        );
+
+        // Go LEFT
+        if (newNode.value < root.value) {
+            console.log(`${newNode.value} < ${root.value} → Go LEFT`);
+
+            if (root.left === null) {
+                console.log(`Left child is empty. Inserting ${newNode.value}`);
+                root.left = newNode;
+            } else {
+                console.log(`Moving to left child (${root.left.value})`);
+                this.insertNode(root.left, newNode);
+            }
+
+        }
+        // Go RIGHT
+        else {
+            console.log(`${newNode.value} >= ${root.value} → Go RIGHT`);
+
+            if (root.right === null) {
+                console.log(`Right child is empty. Inserting ${newNode.value}`);
+                root.right = newNode;
+            } else {
+                console.log(`Moving to right child (${root.right.value})`);
+                this.insertNode(root.right, newNode);
+            }
+        }
+    }
+
+    preOrder(root) {
+        if (root) {
+            console.log(root.value);
+            this.preOrder(root.left);
+            this.preOrder(root.right);
+        }
+    }
+
+    inOrder(root) {
+        if (root) {
+            this.preOrder(root.left);
+            console.log(root.value);
+            this.preOrder(root.right);
+        }
+    }
+
+    postOrder(root) {
+        if (root) {
+            this.preOrder(root.left);
+            this.preOrder(root.right);
+            console.log(root.value);
+        }
+    }
+}
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+
+console.log("pre order traversal: ", bst.preOrder(bst.root));
+console.log("In order traversal: ", bst.inOrder(bst.root));
+console.log("post order traversal: ", bst.postOrder(bst.root));
